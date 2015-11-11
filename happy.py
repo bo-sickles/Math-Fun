f3638268338204bf0b66abc1f4a8f995c83733b0
Bo-Sickles <hitake0507@live.com>
2015-11-11 10:04:05 -0800

    Added happy.py

---
+++ happy.py
@@ -0,0 +1,14 @@
+import sys
+
+def is_happy(number):
+	number = str(number)
+	while len(number) > 1:
+		number.split()
+		result = 0
+		for x in number:
+			result += int(x) ** 2
+		number = str(result)
+	return int(number) == 1
+	
+if __name__ == "__main__":
+	print is_happy(sys.argv[1])
